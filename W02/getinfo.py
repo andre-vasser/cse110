c7efb2314a0ece5b7cf981156eb53842910fc722
andre-vasser <andre.vasser@gmail.com>
2024-04-30 18:42:29 -0600

    getinfo complete

--- W02/getinfo.py
+++ W02/getinfo.py
@@ -1,18 +1,26 @@
 #get information
-first_name = input ('Enter the first name:  ')
-last_name = input ('Enter the last name:  ')
+first_name = input ('Enter the first name:  ').capitalize()
+last_name = input ('Enter the last name:  ').upper()
 email = input ('Enter email address:  ')
 phone_number = input ('Enter the phone number:  ')
 # Format the phone number
 formatted_phone_number = f"({phone_number[:3]}) {phone_number[3:6]}-{phone_number[6:]}"
-job_title = input ('Enter the Job Title:  ')
+job_title = input ('Enter the Job Title:  ').title()
 id_number = input ('Enter the Id Number:  ')
 format_id = f"{id_number[:2]}-{id_number[2:7]}"
-color_hair = input ('Enter color hair:  ')
-color_eyes = input ('Enter color eyes:  ')
-start_month = input ('Enter Start Month:  ')
+color_hair = input ('Enter color hair:  ').capitalize()
+color_eyes = input ('Enter color eyes:  ').capitalize()
+start_month = input ('Enter Start Month:  ').capitalize()
 status = input ('Are you in training?(Yes or No):  ')
 
+# Accept only "Yes" or "No" for training status
+while True:
+    status = input('Are you in training? (Yes/No): ').capitalize()
+    if status == 'Yes' or status == 'No' or status == 'Y' or status == 'N':
+        break
+    else:
+        print(f'Invalid input. Please enter either "Yes" or "No".')
+        
 #print Id Card
 print ('The Id Card is: ')
 print ('-' *40)
